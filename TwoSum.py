39e26c668f40e9d50f0b2a733a21f59896afb49c
Ahmed Amin <89335161+AhmedAm1n@users.noreply.github.com>
2026-05-30 00:11:57 -0500

    two_sum.py
    Added Two Sum solution

---
+++ TwoSum.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        m={}
+
+        for i, x in enumerate(nums):
+
+            y = target - x
+
+            if y in m: 
+                return [m[y],i]
+            
+            m[x]=i
