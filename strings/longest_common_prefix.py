3b37206b3a3db97b28cc90acbfc8ac9c580a4909
Ahmed Amin <89335161+AhmedAm1n@users.noreply.github.com>
2026-06-09 22:55:00 -0500

    Add Longest Common Prefix solution

---
+++ strings/longest_common_prefix.py
@@ -0,0 +1,14 @@
+# Problem: Longest Common Prefix
+# Difficulty: Easy
+
+from typing import List
+
+class Solution:
+    def longestCommonPrefix (self, strs: List[str]) -> str:
+
+        for i in range(len(strs[0])):
+            for s in strs[1:]:
+                if len(s) <= i or s[i] != strs[0][i]:
+                    return strs[0][:i]
+            
+        return strs[0]
