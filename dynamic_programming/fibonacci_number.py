5bca5a8e6a7cc5935f7761cec99e618e967f90c0
Ahmed Amin <89335161+AhmedAm1n@users.noreply.github.com>
2026-06-17 23:47:25 -0500

    Add Fibonacci Number solution

---
+++ dynamic_programming/fibonacci_number.py
@@ -0,0 +1,10 @@
+
+# Problem: Fibonacci Number
+# Difficulty: Easy
+
+class Solution:
+    def fib(self, n:int) -> int:
+        a,b = 0,1
+        for _ in range(n):
+            a,b = b, a + b
+        return a
